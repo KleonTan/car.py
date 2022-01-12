165f3ff08f7415e6808633cecd4b8f2edba53ac8
13694 <shihao.tan@my.jcu.edu.au>
2022-01-12 20:43:55 +0800

    update

--- silver_service_taxi.py
+++ silver_service_taxi.py
@@ -10,7 +10,7 @@ class SilverServiceTaxi(Taxi):
         self.price_per_km *= fanciness
 
     def __str__(self):
-        return "{} plus flagfall of ${:.2f}".format(super().__str__(),
+        return "{} plus flagfall of ${}".format(super().__str__(),
                                                     self.flagfall)
 
     def get_fare(self):
